2f5616ab9c0b5f0f7a763405fb1f2189442919f0
peiyu0916 <wpeiy0916@gmail.com>
2024-01-05 22:48:40 +1100

    Update styles

--- src/components/UpcomingWeather.js
+++ src/components/UpcomingWeather.js
@@ -6,8 +6,8 @@ const DATA = [
     {
         "dt_txt": "2022-08-30 15:00:00",
         "main": {
-          "temp_min": 296.76,
-          "temp_max": 297.87
+          "temp_min": 7.55,
+          "temp_max": 8.55
         },
         "weather": [
           {
@@ -19,8 +19,8 @@ const DATA = [
         "dt_txt": "2022-08-30 18:00:00",
         "main": {
       
-          "temp_min": 292.84,
-          "temp_max": 295.45,
+          "temp_min": 7.55,
+          "temp_max": 8.55,
           
         },
         "weather": [
@@ -36,19 +36,19 @@ const DATA = [
 const Item = (props) => {
     const {dt_txt, min, max, condition} = props;
     return( 
-        <View>
+        <View style={styles.item}>
             <Feather name={'sun'} size={50} color={'white'} />
-            <Text>{dt_txt}</Text>
-            <Text>{min}</Text>
-            <Text>{max}</Text>
+            <Text style={styles.date}>{dt_txt}</Text>
+            <Text style={styles.temp}>{min}</Text>
+            <Text style={styles.temp}>{max}</Text>
         </View>
     )
 }
 
 const UpcomingWeather = () =>{
-    const renderItem = ({item}) => {
-        return (<Item condition = {item.weather[0].main} dt_txt={item.dt_txt} min={item.main.temp_min} max={item.main.temp_max} />)
-    }
+  const renderItem = ({item}) => {
+    return (<Item condition = {item.weather[0].main} dt_txt={item.dt_txt} min={item.main.temp_min} max={item.main.temp_max} />)
+  }
 
     return (
     <SafeAreaView style={styles.container}>
@@ -56,6 +56,7 @@ const UpcomingWeather = () =>{
         <FlatList 
             data ={DATA}
             renderItem={renderItem}
+            keyExtractor={(item) => item.dt_txt}
         />
         
     </SafeAreaView>
@@ -65,8 +66,28 @@ const UpcomingWeather = () =>{
 const styles = StyleSheet.create({
     container:{
         paddingTop: Platform.OS === 'android'? StatusBar.currentHeight : 0,
-        flex:1
+        flex:1,
+        backgroundColor:'red' 
+    },
+    item:{
+      padding:20,
+      marginVertical:8,
+      marginHorizontal:16,
+      flexDirection:'row',
+      justifyContent:'space-around',
+      alignItems:'center',
+      borderWidth:5,
+      backgroundColor:'pink'
+    },
+    temp:{
+      color:'white',
+      fontSize:20
+    },
+    date:{
+      color:'white',
+      fontSize:16
     }
+    
 })
 
 export default UpcomingWeather
